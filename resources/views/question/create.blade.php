89413ab170456aaa94000ffbb7e8b48af1f9f8ea
tung776 <thanhtung776@gmail.com>
2018-12-26 10:36:22 +0700

    lesson 7 - part1

---
+++ resources/views/question/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h2>Ask Question</h2>
+                        <div class="ml-auto">
+                            <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to list of questions</a>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
