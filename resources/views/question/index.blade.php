f511c2d4c708a73c19397a165e4be14e2a46fba8
nguyễn Thanh Tùng <thanhtung776@gmail.com>
2018-12-27 15:41:05 +0700

    fixed errors guest can't read questions list

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -35,11 +35,11 @@
                                 <div class="d-flex align-items-center">
                                     <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                     <div class="ml-auto">
-                                        @if(Auth::user()->can('update-question', $question))
+                                        @if((Auth::user())&&(Auth::user()->can('update-question', $question)))
                                         <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
                                         @endif
                                         
-                                        @if(Auth::user()->can('delete-question', $question))
+                                        @if((Auth::user())&&(Auth::user()->can('delete-question', $question)))
                                         <form class="form-delete" action="{{ route('questions.destroy', $question->id) }}" method="post">
                                             {{method_field('DELETE')}}
                                             @csrf
